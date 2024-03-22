fadaae1d564ba29393bd2bc2f120ae6255a51a2a
DDJar <89435268+DDJar@users.noreply.github.com>
2024-03-22 17:58:54 +0700

    modifile: Set expires cookies

--- be-web-nodejs/midleware/resToken.js
+++ be-web-nodejs/midleware/resToken.js
@@ -2,6 +2,8 @@ var authenticate = require("../authenticate");
 
 async function resToken(req, res, next) {
   var token = authenticate.getToken({ _id: req.user._id });
+  const expirationDate = new Date();
+  expirationDate.setDate(expirationDate.getDate() + 2);
   if(req.user.lastName ===undefined){
     req.user.lastName = " "
   }
@@ -14,9 +16,9 @@ async function resToken(req, res, next) {
     imgAvt: req.user.imgAvt,
     admin: req.user.admin,
   };
-  res.cookie("token", token);
-  res.cookie("info", JSON.stringify(info));
-  res.cookie("userId", JSON.stringify(req.user._id));
+  res.cookie("token", token,{ expires: expirationDate });
+  res.cookie("info", JSON.stringify(info),{ expires: expirationDate });
+  res.cookie("userId", JSON.stringify(req.user._id),{ expires: expirationDate });
   res.redirect(`http://localhost:3000`);
 }
 
