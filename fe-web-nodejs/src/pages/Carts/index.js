97267ec5ce514e7579ce5aa36b63938bcf2d798d
DDJar <89435268+DDJar@users.noreply.github.com>
2024-03-23 13:16:54 +0700

    modifile: CSS Cart index.js

--- fe-web-nodejs/src/pages/Carts/index.js
+++ fe-web-nodejs/src/pages/Carts/index.js
@@ -36,21 +36,21 @@ function Carts() {
                 </svg>
                 Continue Shopping
             </a>
-            <div className=" mx-auto mt-9 bg-slate-100 ">
+            <div className=" mx-auto mt-9 ">
                 <div className="flex shadow-md my-10 border-4 rounded-lg">
-                    <div className="w-3/4 bg-white px-10 py-10 bg-slate-100">
+                    <div className="w-3/4  px-10 py-10 ">
                         <div className="flex justify-center  border-b pb-8">
                             <h1 className="font-semibold text-2xl">Shopping Cart</h1>
                         </div>
                         <div className="flex mt-10 mb-5">
-                            <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">Product Details</h3>
-                            <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Quantity</h3>
-                            <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Price</h3>
-                            <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Total</h3>
+                            <h3 className="font-semibold  text-xs uppercase w-2/5">Product Details</h3>
+                            <h3 className="font-semibold text-center  text-xs uppercase w-1/5 text-center">Quantity</h3>
+                            <h3 className="font-semibold text-center text-xs uppercase w-1/5 text-center">Price</h3>
+                            <h3 className="font-semibold text-center text-xs uppercase w-1/5 text-center">Total</h3>
                         </div>
                         {cartData.map((cartsdata) => {
                             return (
-                                <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5" key={cartsdata.name}>
+                                <div className="flex items-center -mx-8 px-6 py-5" key={cartsdata.name}>
                                     <div className="flex w-2/5">
                                         <div className="w-20">
                                             <img className="h-20" src={cartsdata.image} alt="" />
@@ -58,9 +58,10 @@ function Carts() {
                                         <div className="flex flex-col justify-between ml-4 flex-grow">
                                             <span className="font-bold text-xl">{cartsdata.name}</span>
                                             <span className="text-red-500 text-xl">{cartsdata.type}</span>
+                                            <div>
                                             <a href="/" className="font-semibold hover:text-red-500 text-gray-500 text-lg">
                                                 Remove
-                                            </a>
+                                            </a></div>
                                         </div>
                                     </div>
                                     <div className="flex justify-center w-1/5">
@@ -69,7 +70,7 @@ function Carts() {
                                                 <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
                                             </svg>
                                         </button>
-                                        <input className="mx-2 border text-center w-8" type="text" defaultValue={cartsdata.quantity} />
+                                        <input className="mx-2 text-black border text-center w-8" type="text" defaultValue={cartsdata.quantity} />
                                         <button>
                                             <svg className="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
                                                 <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
